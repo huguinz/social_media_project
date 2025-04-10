74e67e2ab5ce6fe36193e3ec9b921d89f4568725
huguinz <hugolopessouza58@gmail.com>
2025-04-09 22:55:59 -0300

    comments for create recovery password page

--- scripts/scriptPassword.js
+++ scripts/scriptPassword.js
@@ -34,7 +34,8 @@ const postRecoveryUser = async (event) => {
 	if (response.status == 200) {
 		alert('successfully!')
 		console.log(response)
-		window.location.href = '#'
+		window.location.href = '#' //Fazer com que quando a resposta da API der certo, ele limpar o HTML e colocar uma nova caixa para inserir a nova senha
+		//Após fazer isso, validar a atualizacao da senha com o metodo PUT, para assim chamar a funcao a ser criada que vai mandar essa requisicao para API com o argumento do ID do usuario que chega da resposta dessa requisicao
 	} else {
 		errorMessage.innerHTML = '<p> * Invalid e-mail or password key! </p>'
 		console.log(response)
