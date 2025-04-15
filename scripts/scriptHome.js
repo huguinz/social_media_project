0937caadb7ec267f01da5dd92704889db4d1afb6
HUGO LOPES DE SOUZA <24122289@CFP127ALUNO.LOCAL>
2025-04-15 17:28:20 -0300

    codes in progress

--- scripts/scriptHome.js
+++ scripts/scriptHome.js
@@ -1,7 +1,19 @@
 'use strict'
 
-const newStoriesButton = document.getElementById('new_stories')
+const getAllPublications = async () => {
+	const publications = document.getElementById('publications')
+	const url = 'https://back-spider.vercel.app/publicacoes/listarPublicacoes'
+	const response = await fetch(url)
 
-const postNewStories = () => {}
+	if (response.status === 200) {
+		const data = await response.json()
 
-newStoriesButton.addEventListener('click', () => {})
+		data.forEach((item) => {
+			const teste = document.createElement('img')
+			teste.src = item.imagem
+			publications.appendChild(teste)
+		})
+	}
+}
+
+getAllPublications()
