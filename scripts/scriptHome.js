2d39bc3bdd600253f5fc38d6de99166a057bcd85
huguinz <hugolopessouza58@gmail.com>
2025-04-27 01:42:47 -0300

    feat: implement publication form with validation and dynamic user interaction; add sign-out functionality and improve UI layout

--- scripts/scriptHome.js
+++ scripts/scriptHome.js
@@ -1,5 +1,12 @@
 'use strict'
 
+const newPublicationButton = document.getElementById('new_publication')
+const signOut = document.getElementById('sign_out')
+signOut.addEventListener('click', () => {
+	localStorage.removeItem('idUser')
+	window.location.href = '/index.html'
+})
+
 const getAllPublications = async () => {
 	const publications = document.getElementById('publications')
 	const url = 'https://back-spider.vercel.app/publicacoes/listarPublicacoes'
@@ -13,24 +20,33 @@ const getAllPublications = async () => {
 		const data = await response.json()
 
 		data.forEach(async (item) => {
-			const container_img = document.createElement('div')
-			const container_icons = document.createElement('div')
-			const description_publication = document.createElement('div')
+			const containerImg = document.createElement('div')
+			const containerIcons = document.createElement('div')
+			const descriptionPublication = document.createElement('div')
+			const textContainer = document.createElement('div')
 			const nameUser = document.createElement('h2')
+			const locationUser = document.createElement('p')
+			const descriptionUserPublication = document.createElement('p')
 			const images = document.createElement('img')
 			const like = document.createElement('img')
 			const comment = document.createElement('img')
 			const share = document.createElement('img')
-			const profile_image = document.createElement('img')
+			const profileImage = document.createElement('img')
+			const locationIcon = document.createElement('img')
 			const responseUserById = await getUserById(item.idUsuario)
 
-			container_img.id = 'container_img'
-			container_img.dataset.idPublication = item.id
-			container_icons.id = 'container_icons'
-			description_publication.id = 'description_publication'
-			profile_image.src = responseUserById.imagemPerfil
-			profile_image.id = 'profile_image'
+			containerImg.id = 'container_img'
+			containerImg.dataset.idPublication = item.id
+			containerIcons.id = 'container_icons'
+			descriptionPublication.id = 'description_publication'
+			textContainer.id = 'text_container'
+			profileImage.src = responseUserById.imagemPerfil
+			profileImage.id = 'profile_image'
 			nameUser.textContent = responseUserById.nome
+			locationIcon.src = '/src/img/location_icon.png'
+			locationIcon.id = 'location_icon'
+			locationUser.textContent = item.local
+			descriptionUserPublication.textContent = item.descricao
 			images.src = item.imagem
 			images.id = 'images_publications'
 			like.src = '/src/img/outline_like.png'
@@ -40,15 +56,34 @@ const getAllPublications = async () => {
 			share.src = '/src/img/share.png'
 			share.id = 'share'
 
-			publications.appendChild(container_img)
-			description_publication.appendChild(profile_image)
-			description_publication.appendChild(nameUser)
-			container_img.appendChild(description_publication)
-			container_img.appendChild(images)
-			container_icons.appendChild(like)
-			container_icons.appendChild(comment)
-			container_icons.appendChild(share)
-			container_img.appendChild(container_icons)
+			publications.appendChild(containerImg)
+			descriptionPublication.appendChild(profileImage)
+			textContainer.appendChild(nameUser)
+			textContainer.appendChild(locationIcon)
+			textContainer.appendChild(locationUser)
+			descriptionPublication.appendChild(textContainer)
+			containerImg.appendChild(descriptionPublication)
+			containerImg.appendChild(descriptionUserPublication)
+			containerImg.appendChild(images)
+			containerIcons.appendChild(like)
+			containerIcons.appendChild(comment)
+			containerIcons.appendChild(share)
+			containerImg.appendChild(containerIcons)
+
+			if (item.idUsuario === localStorage.getItem('idUser')) {
+				const trashIconContainer = document.createElement('div')
+				const trashIcon = document.createElement('img')
+
+				trashIconContainer.id = 'trash_icon_container'
+				trashIcon.src = '/src/img/trash.png'
+
+				trashIconContainer.appendChild(trashIcon)
+				textContainer.appendChild(trashIconContainer)
+
+				trashIconContainer.addEventListener('click', async () => {
+					await deletePublication(item.id)
+				})
+			}
 
 			if (item.curtidas !== undefined) {
 				item.curtidas.forEach((likes) => {
@@ -62,7 +97,7 @@ const getAllPublications = async () => {
 			}
 
 			like.addEventListener('click', () => {
-				const getDataSet = container_img.dataset.idPublication
+				const getDataSet = containerImg.dataset.idPublication
 				like.src = '/src/img/fill_like.png'
 				like.classList.add('like_fill')
 				putLikeUser(getDataSet)
@@ -101,5 +136,92 @@ const getUserById = async (id) => {
 
 	return data
 }
+
+const postPublication = async () => {
+	const description = document.getElementById('description').value
+	const location = document.getElementById('location').value
+	const urlImage = document.getElementById('url_image').value
+	const idUser = localStorage.getItem('idUser')
+	const url = 'https://back-spider.vercel.app/publicacoes/cadastrarPublicacao'
+
+	const body = {
+		descricao: description,
+		dataPublicacao: currentData(),
+		imagem: urlImage,
+		local: location,
+		idUsuario: idUser
+	}
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(body)
+	}
+
+	const response = await fetch(url, options)
+
+	if (response.status === 201) {
+		alert('publication added successfully!')
+		window.location.reload()
+	} else if (response.status === 400) {
+		alert('all fields are mandatory!')
+	} else {
+		alert('an unexpected error occurred while trying to add the publication!')
+	}
+}
+
+const deletePublication = async (id) => {
+	const url = `https://back-spider.vercel.app/publicacoes/deletarPublicacao/${id}`
+	const options = {
+		method: 'DELETE'
+	}
+
+	const response = await fetch(url, options)
+
+	if (response.status === 200) {
+		alert('publication deleted successfully!')
+	}
+}
+
+const currentData = () => {
+	const date = new Date()
+	const day = date.getDay()
+	const month = date.getMonth() + 1
+	const year = date.getFullYear()
+	const newDate = `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`
+
+	return newDate
+}
+
+const newPublicationData = () => {
+	const newPublicationContainer = document.querySelector('article')
+	const containerForm = document.getElementById('container_form')
+	const inputDescription = document.getElementById('description')
+	const charactersNumber = containerForm.querySelector('p')
+	const closeTab = document.getElementById('close_tab')
+	newPublicationContainer.style.pointerEvents = 'all'
+	newPublicationContainer.style.opacity = '1'
+
+	closeTab.addEventListener('click', () => {
+		newPublicationContainer.style.pointerEvents = 'none'
+		newPublicationContainer.style.opacity = '0'
+	})
+
+	inputDescription.addEventListener('input', () => {
+		charactersNumber.textContent = inputDescription.value.length
+
+		if (inputDescription.value.length >= 80) {
+			charactersNumber.style.color = 'red'
+			inputDescription.style.borderBottomColor = '#df3636'
+		} else {
+			charactersNumber.style.color = 'black'
+			inputDescription.style.borderBottomColor = 'black'
+		}
+	})
+}
+
+newPublicationButton.addEventListener('click', newPublicationData)
 
 getAllPublications()
